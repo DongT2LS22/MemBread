a75edf967db5f948484a65f89ed3cba4a6435e61
DongT2LS <vanhein445566@gmail.com>
2023-11-01 21:02:12 +0700

    update homepage rank page

--- lib/widget/molecules/card/profile_card.dart
+++ lib/widget/molecules/card/profile_card.dart
@@ -11,30 +11,31 @@ class ProfileCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           direction: Axis.horizontal,
           children: [
-            Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: AssetImage('assets/elon-musk.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  border: const Border(
-                    bottom: BorderSide(color: Colors.grey),
-                    top: BorderSide(color: Colors.grey),
-                    left: BorderSide(color: Colors.grey),
-                    right: BorderSide(color: Colors.grey),
-                  ),
-                  borderRadius: BorderRadius.circular(50)),
-            ),
-            const SizedBox(width: 20,),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Elon musk",style: Theme.of(context).textTheme.titleSmall,overflow: TextOverflow.ellipsis,),
-                Text("200",style: Theme.of(context).textTheme.titleSmall,)
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      image: const DecorationImage(
+                        image: AssetImage('assets/elon-musk.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      border: const Border(
+                        bottom: BorderSide(color: Colors.grey),
+                        top: BorderSide(color: Colors.grey),
+                        left: BorderSide(color: Colors.grey),
+                        right: BorderSide(color: Colors.grey),
+                      ),
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text("Elon Musk",style: Theme.of(context).textTheme.titleSmall,),
+                )
               ],
-            )
+            ),
+            Text("200",style: Theme.of(context).textTheme.titleSmall,)
           ],
         )
     );

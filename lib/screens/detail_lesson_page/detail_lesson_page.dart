68962dc275f28a568807cc95ae4a02991a087da4
DongT2LS <vanhein445566@gmail.com>
2023-11-05 21:11:50 +0700

    update lesson detail

--- lib/screens/detail_lesson_page/detail_lesson_page.dart
+++ lib/screens/detail_lesson_page/detail_lesson_page.dart
@@ -11,47 +11,69 @@ class DetailLessonPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: CloseTitleAppbar(onPressed:() => Navigator.pop(context),title: "Hello",),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 70,
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              children: [
-                Container(
-                  color:Theme.of(context).primaryColor,
-                  width: 60,
-                  height: 70,
-                  child: Center(
-                    child : Text("1",style: Theme.of(context).textTheme.titleLarge,)
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15),
-                      child: Column(
-                        children: [
-                          Text("Hello",style: Theme.of(context).textTheme.titleMedium,),
-                          Text("1/100",style: Theme.of(context).textTheme.displayMedium,),
-                        ],
-                      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 70,
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  Container(
+                    color:Theme.of(context).primaryColor,
+                    width: 60,
+                    height: 70,
+                    child: Center(
+                      child : Text("1",style: Theme.of(context).textTheme.titleLarge,)
                     ),
-                    ProgressSlider(progress: 0.1, width: MediaQuery.of(context).size.width-60,borderRadius: 0,)
-                  ],
-                )
-              ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Column(
+                          children: [
+                            Text("Hello",style: Theme.of(context).textTheme.titleMedium,),
+                            Text("1/100",style: Theme.of(context).textTheme.displayMedium,),
+                          ],
+                        ),
+                      ),
+                      ProgressSlider(progress: 0.1, width: MediaQuery.of(context).size.width-60,borderRadius: 0,)
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-          VocaCard(voca: "Hello", mean: "Xin chao",level: 0,),
-          const SizedBox(height: 5,),
-          VocaCard(voca: "Hello", mean: "Xin chao",level: 4,),
-          const SizedBox(height: 5,),
-          VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
-          const SizedBox(height: 5,),
-        ],
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 0,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 4,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+            VocaCard(voca: "Hello", mean: "Xin chao",level: 2,),
+            const SizedBox(height: 5,),
+          ],
+        ),
       ),
     );
   }

ed991bc5fe507c2300bcee448aeea186662c9955
DongT2LS <vanhein445566@gmail.com>
2024-03-16 16:01:29 +0700

    change home

--- lib/screens/streak_page/streak_page.dart
+++ lib/screens/streak_page/streak_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gr1_flutter/widget/organisms/app_bar/none_title_appbar.dart';
+import 'package:gr1_flutter/screens/home_page_remake/home_page.dart';
+import 'package:gr1_flutter/widget/atoms/buttons/border_button.dart';
+import 'package:gr1_flutter/widget/atoms/buttons/light_button.dart';
+import 'package:gr1_flutter/widget/atoms/buttons/primary_button.dart';
 
 class StreakPage extends StatelessWidget {
   const StreakPage({super.key});
@@ -8,21 +11,38 @@ class StreakPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: NonTitleAppBar(onPressed:() => Navigator.pop(context),),
+      appBar: AppBar(),
       body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
         width: MediaQuery.of(context).size.width,
         child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const SizedBox(height: 20,),
               Container(
                 height: 200,
                 width: 200,
                 decoration: BoxDecoration(
-                    border: Border.all(width: 10,color: Colors.white),
+                    border: Border.all(width: 5,color: Colors.white),
                     borderRadius: BorderRadius.circular(200)
                 ),
                 child: Image.asset("assets/streak.png"),
               ),
+              Text("5 days straight!",style: Theme.of(context).textTheme.titleLarge,),
+              Text("Increases if you practice every day and will return to zero if you skip a day !",style: Theme.of(context).textTheme.titleMedium,textAlign: TextAlign.center,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LightButton(
+                    width: MediaQuery.of(context).size.width/2 - 40,
+                    child: Center(child: Text("Share",style: Theme.of(context).textTheme.labelMedium,),),
+                  ),
+                  PrimaryButton(
+                    onPressed:() => Navigator.push(context,MaterialPageRoute(builder: (context)=>HomePage())),
+                    width: MediaQuery.of(context).size.width/2 - 40,
+                    child: Center(child: Text("Continue",style: Theme.of(context).textTheme.labelMedium,),),
+                  )
+                ],
+              )
             ]
         ),
       ),
